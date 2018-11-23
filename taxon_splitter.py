3a8efb5053fb4ed3640e2121f7ad2eb670b7c9f9
vortacs <jtoscanifield@ucmerced.edu>
2018-11-23 15:35:30 -0800

    working on splitting file names

--- taxon_splitter.py
+++ taxon_splitter.py
@@ -2,20 +2,24 @@
 
 import os
 import argparse
+import shutil
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='descp')
     parser.add_argument('--taxa_dir')
-    #parser.add_argument('--new_dir')
+    parser.add_argument('--new_dir')
     parser.add_argument('--max_num')
     return parser.parse_args()
 
 args = parse_args()
 files = os.listdir(args.taxa_dir)
+os.chdir(args.taxa_dir)
 
 for i in files:
+    print(i)
     splitter = i.split('_')
-    num = int(splitter[1])
-    if num <= int(args.max_num):
-        print(num)
+    num = splitter[1]
+    print(num)
+    #if num <= float(args.max_num):
+        #shutil.move('./' + i, args.new_dir+i)
