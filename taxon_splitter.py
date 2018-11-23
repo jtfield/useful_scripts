73955f638d81f47e97a83dc43a171e46dd376fde
vortacs <jtoscanifield@ucmerced.edu>
2018-11-23 15:39:59 -0800

    working on splitting

--- taxon_splitter.py
+++ taxon_splitter.py
@@ -15,11 +15,11 @@ def parse_args():
 args = parse_args()
 files = os.listdir(args.taxa_dir)
 os.chdir(args.taxa_dir)
+num = int(args.max_num)
 
 for i in files:
     print(i)
     splitter = i.split('_')
-    num = splitter[1]
-    print(num)
-    #if num <= float(args.max_num):
+    taxon_num = splitter[1]
+    if taxon_num <= num:
         #shutil.move('./' + i, args.new_dir+i)
