4e1988cf4953dbf9a4b234c8c82d3ef9f3b92e31
vortacs <jtoscanifield@ucmerced.edu>
2018-12-10 12:58:35 -0800

    added functionality for input of any samfile

--- one_off_search.py
+++ one_off_search.py
@@ -1,22 +1,37 @@
 #! /usr/bin/env python
 
-#file = open('full_test_best_map.sam','r')
-#file = open('test_best_map.sam','r')
+import argparse
 
-#with open('full_test_best_map.sam') as file:
-with open('best_map.sam') as file:
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--sam_file')
+	parser.add_argument('--gt_num')
+	parser.add_argument('--lt_num')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    with_open(args.sam_file) as file:
+	read_position_start = args.gt_num
+	read_position_end = args.lt_num
+
+
+#with open('best_map.sam') as file:
 
 #readdat = file.read()
 
-	next(file)
-	next(file)
-	next(file)
-	red = file.readlines()
-	for i in red:
-		line_split = i.split('\t')	
-		pos = line_split[3:4]
-		for item in pos:
-			num = int(item)
-	
-			if num > 1830415 and num < 1830454:
-				print(i)
+		next(file)
+		next(file)
+		next(file)
+		red = file.readlines()
+		for i in red:
+			line_split = i.split('\t')
+			pos = line_split[3:4]
+			for item in pos:
+				num = int(item)
+
+				# if num > 1830415 and num < 1830454:
+				# 	print(i)
+				if num > read_position_start and num < read_position_end:
+					print(i)
