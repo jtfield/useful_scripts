5bae79c25a8a54984d99246aa8a93b4cbad8384c
vortacs <jtoscanifield@ucmerced.edu>
2019-01-28 16:38:00 -0800

    added comments to explain portions of the code. also replacing names of fasta with original names as intended

--- line_printer.sh
+++ line_printer.sh
@@ -3,8 +3,12 @@
 file=$1
 secondfile=$2
 
+# initialize variables for the numbers that will be compared to eachother
 first_num=''
 second_num=''
+
+# read the fasta file and loop through it
+# looking for lines that start with > indicating a contig name
 cat $file | while read line;
 do
 	#echo "$line"
@@ -12,9 +16,18 @@ do
 	if [[ $line = \>* ]]
 	then
 		#echo "$line"
+		
+		# if a carrot ( > ) is found on the line
+		# check if there is a number in that line surrounded by two _'s
+		# example: "taxon_5_1.fq"
+
 		if [[ $line = *_[1-9]_* ]]
 		then
 			#echo "$line has a number"
+
+
+			# if a number is found, strip away the first portion of the name and the last portion of the name
+
 			extractedNum="${line#*_}"     # Remove through first :
 			extractedNum="${extractedNum%%_*}"  # Remove from next : to end of string
 			#echo "$extractedNum"
@@ -22,6 +35,12 @@ do
 			#echo "begin first_num"
 			#echo "$first_num"
 			#echo "end first_num"
+
+			# after stripping away the non-numeric portions of the name
+			# loop through the file with the original names
+			# seperate the number in the name and compare those numbers to eachother to find matches
+			# if they match, replace the name in the fasta with the original name
+
 			cat $secondfile | while read secondline;
                         do
                                 second_extract_num="${secondline#*_}"
@@ -32,9 +51,9 @@ do
 				if [[ $first_num == $second_num ]]
 				then
 					echo "match"
-					echo "replacing $secondline with $line"
+					echo "replacing $line with $secondline"
 					
-					sed -i -e "s/$secondline/$line/g" $secondfile
+					sed -i -e "s/$line/$secondline/g" $file
 				else
 					echo "no match"
 				fi
